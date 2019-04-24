239b56ff88a243b4906dede89145b0868792c747
ray chung <yj4004@hotmail.com>
2019-04-23 18:46:52 -0700

    modals

---
+++ client/src/Modals/UploadModal.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+
+class UploadModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      file: URL.createObjectURL(e.target.files[0])
+    });
+  }
+
+  /** 
+  appendToUserBookListings() {
+    var bookListings = [];
+  }
+  */
+
+  render() {
+    return (
+      <div>
+        <div className="modal" id="uploadModal" data-backdrop="false">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h3 className="text-center text-primary centerMe">
+                  Upload Book
+                </h3>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <form>
+                  <div className="row">
+                    <div className="col">
+                      <input
+                        type="text"
+                        id="title"
+                        className="form-control"
+                        placeholder="Title"
+                      />
+                      <br />
+                      <input
+                        type="text"
+                        id="price"
+                        className="form-control"
+                        placeholder="Price"
+                      />
+                      <br />
+                      <input
+                        type="text"
+                        id="course"
+                        className="form-control"
+                        placeholder="Course"
+                      />
+                      <br />
+                      <input
+                        type="text"
+                        id="description"
+                        className="form-control"
+                        placeholder="Description"
+                      />
+                      <br />
+                      <input
+                        className="btn"
+                        type="file"
+                        onChange={this.handleChange}
+                      />
+                      <br />
+                      <img
+                        style={{ width: 320, height: 320 }}
+                        src={this.state.file}
+                        resizemode="contain"
+                        alt="book"
+                      />
+                    </div>
+                  </div>
+                  <br />
+                  <button className="btn btn-primary" type="submit">
+                    Upload
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div id="modal-backdrop" className="modal-backdrop-transparent" />
+        </div>
+      </div>
+    );
+  }
+}
+export default UploadModal;
--- client/src/Pages/Books.js
+++ client/src/Pages/Books.js
@@ -2,76 +2,185 @@ import React, { Component } from "react";
 import "../App.css";
 import Card from "../Components/Card.js";
 import Book from "../Components/Book.js";
+import UploadModal from "../Modals/UploadModal.js";
 
 class Books extends Component {
-
-  // constructor(props){
-  //   super(props)
-  //   this.state = {
-  //     file: null
-
-  //   }
-  //   this.handleChange = this.handleChange.bind(this)
-  // }
-
-  constructor(props){
-    super();
-    this.state={
-        books:[]
-
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      page: 1,
+      error: false
+    };
+    this.updateBooks = this.updateBooks.bind(this);
   }
 
   componentDidMount() {
-    fetch('/books')
+    fetch("/books")
       .then(res => res.json())
-      .then(books => this.setState({books}, () => console.log(books)));
+      .then(books => this.setState({ books }, () => console.log(books)))
+      .catch(error => {
+        console.error(error);
+        this.setState({
+          error: true
+        });
+      });
   }
 
+  renderBooks = () => {
+    const { books, page } = this.state;
+    //console.log("DisplatAmt:" + this.state.displayAmt);
+    //console.log(books);
+    var table = [];
+    var booksPerRow = 4;
 
-  // handleChange(e) {
-  //   this.setState({
-  //     file: URL.createObjectURL(e.target.files[0])
-  //   })
-  // }
+    for (var i = 0; i < page; i++) {
+      table.push(
+        <div className="card-deck" key={i} style={{ paddingBottom: "10px" }}>
+          {books
+            //.filter(book => book < page)
+            .slice(i * booksPerRow, booksPerRow + i * booksPerRow)
+            .map(book => (
+              <Book
+                key={book._id}
+                price={book.price}
+                title={book.title}
+                image={book.image}
+                course={null}
+                description={book.description}
+                contactInfo={null}
+              />
+            ))}
+        </div>
+      );
+    }
+    return table;
+  };
 
-  
+  updateBooks() {
+    this.setState(prev => {
+      return { page: prev.page + 1 };
+    });
+  }
 
   render() {
     return (
       <div>
-
-
-
-      <div>
-      <form>
-                <div className="row">
-                  <div className="col">
-                    <input type="text" name="title" className="form-control" placeholder="Title"/><br/>
-                    <input type="text" name="price" className="form-control" placeholder="Price"/><br/>
-                    <input type="text" name="course" className="form-control" placeholder="Course"/><br/>
-                    <input type="text" name="description" className="form-control" placeholder="Description"/><br/>
-                    <input type="file" onChange={this.handleChange}/>
-                    <img style={{ width:250, height:250 }} src={this.state.file} resizeMode='contain'/><br/>
-                  </div>
-                </div>
-                <button className="btn btn-primary" onClick={() => {} } type="submit">Upload</button>
-              </form>
+        <UploadModal />
+        <div
+          className="container-fluid"
+          id="container-scroll"
+          style={{ padding: "0px 80px " }}
+        >
+          <div className="col-lg-12">
+            <Card
+              title="Books"
+              upload={
+                <button
+                  style={{ float: "right", margin: "10px" }}
+                  className="btn btn-primary"
+                  data-target="#uploadModal"
+                  data-toggle="modal"
+                >
+                  Upload
+                </button>
+              }
+              height="55rem"
+              body={this.renderBooks()}
+              footer={
+                <button
+                  style={{ margin: 20 }}
+                  className="btn btn-primary"
+                  onClick={() => this.updateBooks()}
+                >
+                  Load More
+                </button>
+              }
+            />
+          </div>
+        </div>
       </div>
+    );
+  }
+}
 
-      <div>
-          <h2>Books</h2>
-          <ul>
-              {this.state.books.map(book => 
-              <li key={book.id}>{book.title} {book.description}</li>
-              )}
-        </ul>
-      </div>
+export default Books;
 
 
+// import React, { Component } from "react";
+// import "../App.css";
+// import Card from "../Components/Card.js";
+// import Book from "../Components/Book.js";
+
+// class Books extends Component {
+
+//   // constructor(props){
+//   //   super(props)
+//   //   this.state = {
+//   //     file: null
+
+//   //   }
+//   //   this.handleChange = this.handleChange.bind(this)
+//   // }
+
+//   constructor(props){
+//     super();
+//     this.state={
+//         books:[]
+
+//     }
+//   }
+
+//   componentDidMount() {
+//     fetch('/books')
+//       .then(res => res.json())
+//       .then(books => this.setState({books}, () => console.log(books)));
+//   }
 
 
-      </div>
+//   // handleChange(e) {
+//   //   this.setState({
+//   //     file: URL.createObjectURL(e.target.files[0])
+//   //   })
+//   // }
+
+  
+
+//   render() {
+//     return (
+//       <div>
+
+
+
+//       <div>
+//       <form>
+//                 <div className="row">
+//                   <div className="col">
+//                     <input type="text" name="title" className="form-control" placeholder="Title"/><br/>
+//                     <input type="text" name="price" className="form-control" placeholder="Price"/><br/>
+//                     <input type="text" name="course" className="form-control" placeholder="Course"/><br/>
+//                     <input type="text" name="description" className="form-control" placeholder="Description"/><br/>
+//                     <input type="file" onChange={this.handleChange}/>
+//                     <img style={{ width:250, height:250 }} src={this.state.file} resizeMode='contain'/><br/>
+//                   </div>
+//                 </div>
+//                 <button className="btn btn-primary" onClick={() => {} } type="submit">Upload</button>
+//               </form>
+//       </div>
+
+//       <div>
+//           <h2>Books</h2>
+//           <ul>
+//               {this.state.books.map(book => 
+//               <li key={book.id}>{book.title} {book.description}</li>
+//               )}
+//         </ul>
+//       </div>
+
+
+
+
+//       </div>
       
 
 
@@ -79,62 +188,62 @@ class Books extends Component {
 
 
 
-      // <div>
-      // <div className="modal" id="uploadModal" data-backdrop="false">
-      //   <div className="modal-dialog">
-      //     <div className="modal-content">
-      //       <div className="modal-header">
-      //         <h3 className="text-center text-primary centerMe"> Upload Book </h3>
-      //         <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      //       </div>
+//       // <div>
+//       // <div className="modal" id="uploadModal" data-backdrop="false">
+//       //   <div className="modal-dialog">
+//       //     <div className="modal-content">
+//       //       <div className="modal-header">
+//       //         <h3 className="text-center text-primary centerMe"> Upload Book </h3>
+//       //         <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+//       //       </div>
             
-      //     </div>
-      //   </div>
-      // </div>
-      // <div className="container-fluid" id="container-scroll">
-      //   <div className="col-lg-12">
-      //     <Card
-      //       title="Books"
-      //       upload={<button style={{float:"right", margin:"10px"}} className="btn btn-primary" data-target="#uploadModal" data-toggle="modal"> Upload </button>}
-      //       height="55rem"
-      //       body={
-      //         <div className="card-deck">
-      //           <Book
-      //             price="100"
-      //             title="Intro to Operating Systems"
-      //             course="COMP 322"
-      //             description="Hard-Cover book, bought brand-new and used for a semester. Email if interested only"
-      //             contactInfo="someonesPrivate@email.com"
-      //           />
-      //           <Book
-      //             price="79"
-      //             title="Data Mining"
-      //             course="COMP 484"
-      //             description="Selling as a SET 'Psychological Assessment with the MMPI-2' Alan F. Friedman, Richard Lewak, David S. Nicols, James T. Webb 'Essentials of the PAI Assessment' Leslie C. ..."
-      //             contactInfo="someoneElse@email.com"
-      //           />
-      //           <Book
-      //             price="30"
-      //             title="Combinatorial Algorithms"
-      //             course="COMP 482"
-      //             description="First come first serve"
-      //             contactInfo="anotherSomeone@email.com"
-      //           />
-      //           <Book
-      //             price="55"
-      //             title="Discrete Structures"
-      //             course="COMP 252"
-      //             description="Have many other books for sale"
-      //             contactInfo="yes@email.com"
-      //           />
-      //         </div>
-      //       }
-      //     />
-      //   </div>
-      // </div>
-      // </div>
-    );
-  }
-}
+//       //     </div>
+//       //   </div>
+//       // </div>
+//       // <div className="container-fluid" id="container-scroll">
+//       //   <div className="col-lg-12">
+//       //     <Card
+//       //       title="Books"
+//       //       upload={<button style={{float:"right", margin:"10px"}} className="btn btn-primary" data-target="#uploadModal" data-toggle="modal"> Upload </button>}
+//       //       height="55rem"
+//       //       body={
+//       //         <div className="card-deck">
+//       //           <Book
+//       //             price="100"
+//       //             title="Intro to Operating Systems"
+//       //             course="COMP 322"
+//       //             description="Hard-Cover book, bought brand-new and used for a semester. Email if interested only"
+//       //             contactInfo="someonesPrivate@email.com"
+//       //           />
+//       //           <Book
+//       //             price="79"
+//       //             title="Data Mining"
+//       //             course="COMP 484"
+//       //             description="Selling as a SET 'Psychological Assessment with the MMPI-2' Alan F. Friedman, Richard Lewak, David S. Nicols, James T. Webb 'Essentials of the PAI Assessment' Leslie C. ..."
+//       //             contactInfo="someoneElse@email.com"
+//       //           />
+//       //           <Book
+//       //             price="30"
+//       //             title="Combinatorial Algorithms"
+//       //             course="COMP 482"
+//       //             description="First come first serve"
+//       //             contactInfo="anotherSomeone@email.com"
+//       //           />
+//       //           <Book
+//       //             price="55"
+//       //             title="Discrete Structures"
+//       //             course="COMP 252"
+//       //             description="Have many other books for sale"
+//       //             contactInfo="yes@email.com"
+//       //           />
+//       //         </div>
+//       //       }
+//       //     />
+//       //   </div>
+//       // </div>
+//       // </div>
+//     );
+//   }
+// }
 
-export default Books;
+// export default Books;
