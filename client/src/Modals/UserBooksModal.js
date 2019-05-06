2de4809a3baf6693e10176910ba581ffb639a5f7
Surinder Singh <surindersingh9495@gmail.com>
2019-05-05 22:46:50 -0700

    Pre Ray State
    did "npm rebuild bcrypt --build-from-source" after pulling

--- client/src/Modals/UserBooksModal.js
+++ client/src/Modals/UserBooksModal.js
@@ -70,21 +70,15 @@ class UserBooksModal extends Component {
               </div>
               <div className="modal-body">
                 <div className="card-deck">
-                  {this.renderUserBooks()}
-                  {/* <Book
+  
+                  { <Book
                     price="100"
                     title="Intro to Operating Systems"
                     course="COMP 322"
                     description="Hard-Cover book, bought brand-new and used for a semester. Email if interested only"
                     contactInfo="someonesPrivate@email.com"
                   />
-                  <Book
-                    price="79"
-                    title="Data Mining"
-                    course="COMP 484"
-                    description="Selling as a SET 'Psychological Assessment with the MMPI-2' Alan F. Friedman, Richard Lewak, David S. Nicols, James T. Webb 'Essentials of the PAI Assessment' Leslie C. ..."
-                    contactInfo="someoneElse@email.com"
-                  /> */}
+                 }
                 </div>
               </div>
             </div>
